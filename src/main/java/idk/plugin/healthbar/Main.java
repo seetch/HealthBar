11ed0cf52f691d2169b1f0f93c35702ccc30b309
Mikhail Sergeevich <seetch@inbox.ru>
2023-09-07 22:54:30 +0300

    Add some checks

--- src/main/java/idk/plugin/healthbar/Main.java
+++ src/main/java/idk/plugin/healthbar/Main.java
@@ -2,6 +2,7 @@ package idk.plugin.healthbar;
 
 import cn.nukkit.Player;
 import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.potion.Effect;
 import cn.nukkit.scheduler.NukkitRunnable;
 
 public class Main extends PluginBase {
@@ -13,7 +14,9 @@ public class Main extends PluginBase {
             public void run() {
                 try {
                     for (Player p : getServer().getOnlinePlayers().values()) {
-                        p.setScoreTag("\u00A7f" + Math.floor(p.getHealth()) + " \u00A7c❤");
+                        if ((p.isSurvival() || p.isAdventure()) && !p.hasEffect(Effect.INVISIBILITY)) {
+                            p.setScoreTag("\u00A7f" + Math.floor(p.getHealth()) + " \u00A7c❤");
+                        }
                     }
                 } catch (Exception ignore) {
                 }
